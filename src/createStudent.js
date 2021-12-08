e139811da0515127cc217ce7b09380cad1b62fb1
Juliano Boese <juliano.boese@gmail.com>
2021-12-08 17:32:44 -0300

    Requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,6 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (studentName) => ({ name: studentName, feedback: () => 'Eita pessoa boa!' });
 
 module.exports = createStudent;
